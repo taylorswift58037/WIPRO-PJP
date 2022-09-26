a7d61610f64c3fba235ecec09d4619adcd53999f
Code For Fun <105599054+taylorswift58037@users.noreply.github.com>
2022-09-26 19:19:56 +0530

    Add files via upload

---
+++ DotNet/Exercise 1/Exersice1/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Exersice1
+{
+    
+        public class Student
+        {
+            public int StudentId { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class Course
+        {
+            public int CourseId { get; set; }
+            public string CourseName { get; set; }
+        }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolContext())
+            {
+
+                var std = new Student()
+                {
+                    Name = "Bill"
+                };
+
+                context.Students.Add(std);
+                context.SaveChanges();
+            }
+        }
+    }
+
+}
