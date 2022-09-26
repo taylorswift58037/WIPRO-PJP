bf51fd46b01e8bd1b462ad2c6b9759217748f465
Code For Fun <105599054+taylorswift58037@users.noreply.github.com>
2022-09-26 20:02:17 +0530

    Add files via upload

---
+++ DotNet/RandomHelper/RandomHelper/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RandomHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers");
+            int min = Convert.ToInt32(Console.ReadLine());
+            int max = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(RandomHelper.randint(min,max));
+            Console.WriteLine(RandomHelper.randdouble(min, max));
+        }
+    }
+}
---
+++ DotNet/Salary/Salary/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Salary
+{
+    class empsalary
+    {
+        String Name;
+        double Basepay,HRA,GROSS,DA,TAX,NetPay;
+
+        public void caldetails(String name,double bas)
+        {
+            this.Name = name;
+            this.Basepay=bas;
+            this.HRA = 0.15 * bas;
+            this.DA = 0.10 * bas;
+            this.GROSS = this.Basepay + this.DA + this.HRA;
+            this.TAX = 0.08 * this.GROSS;
+            this.NetPay = this.GROSS - this.TAX;   
+        }
+        public void displaySalary()
+        {
+            Console.WriteLine("NAME      {0}\nBASE PAY  {1}\nHRA       {2}\nDA        {3}\nGROSS PAY {4}\nTAX       {5}\nNET PAY   {6}",this.Name,this.Basepay,this.HRA,this.DA,this.GROSS,this.TAX,this.NetPay);
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Name and BasePay");
+            empsalary emp1 = new empsalary();
+            emp1.caldetails(Console.ReadLine(), Convert.ToDouble(Console.ReadLine()));
+            emp1.displaySalary();
+        }
+    }
+}
